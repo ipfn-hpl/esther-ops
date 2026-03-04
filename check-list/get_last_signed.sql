19592e6ccaabfc3bbe032c8e51a62f6d122dc67e
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2026-03-04 18:50:18 +0000

    Added stored function
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- check-list/get_last_signed.sql
+++ check-list/get_last_signed.sql
@@ -1,38 +1,26 @@
-CREATE OR REPLACE FUNCTION get_last_signed(p_report_id INT, p_subsystem_id INT, p_role_id INT, p_limit INT DEFAULT 10)
-RETURNS TABLE (
-      item_id INT, 
-      seq_order SMALLINT, 
-      time_date TIMESTAMP,
-      resp TEXT,
-      c_status TEXT, 
-      item_name  TEXT
+CREATE OR REPLACE FUNCTION get_last_signed(
+  p_report_id INT, 
+  p_subsystem_id INT,
+  p_role_id   INT,
+  OUT item_id  INT,
+  OUT seq_order  INT
 )
 LANGUAGE plpgsql
-STABLE
 AS $$
 BEGIN
-    RETURN QUERY
-      SELECT
-        c.item_id, 
-        i.seq_order, 
-        c.time_date, 
-        r.short_name AS resp, 
-        s.status AS c_status, 
 
-        i.name AS item_name
-      FROM (
-        SELECT inner_c.item_id, inner_c.time_date, inner_c.complete_status_id
-        FROM complete inner_c
-        WHERE report_id = p_report_id
-        ORDER BY time_date DESC
-        LIMIT p_limit
-        ) c
-        INNER JOIN item i ON c.item_id = i.id 
-        INNER JOIN role r ON i.role_id = r.id 
-        INNER JOIN complete_status s ON c.complete_status_id = s.id 
-        WHERE i.subsystem_id = p_subsystem_id AND i.role_id = p_role_id
-        ORDER BY c.time_date ASC;
-        -- WHERE c.report_id = p_report_id AND i.subsystem_id = p_subsystem_id
+    SELECT c.item_id
+    INTO item_id
+    FROM complete c
+    INNER JOIN item i ON c.item_id = i.id
+    WHERE c.report_id = p_report_id AND
+    i.subsystem_id = p_subsystem_id AND
+    i.role_id = p_role_id
+    ORDER BY time_date DESC LIMIT 1;
+
+    SELECT item.seq_order 
+    INTO seq_order
+    FROM item
+    WHERE id = item_id;
 END;
 $$;
-
